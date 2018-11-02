e01fc91c6a6b4a9fbbd0337da8188880fa57a791
Flaque <econrad@zagmail.gonzaga.edu>
2018-11-01 22:13:17 -0700

    Refactor into fs

---
+++ fs.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"runtime"
+)
+
+const freakfile = ".freak.csv"
+
+func homeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
+}
+
+func createOrGetFile(name string) (*os.File, error) {
+	var file *os.File
+	var err error
+	if fileExists(name) {
+		file, err = os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	} else {
+		file, err = os.Create(name)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// WriteToCSV records a row to the file path. It'll create the file if it doesn't exist
+func WriteToCSV(data []string, path string) error {
+
+	// Create freak file if not exists, otherwise read
+	file, err := createOrGetFile(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write csv line at the end
+	err = writer.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
 	"path"
-	"runtime"
 	"strconv"
 	"time"
 )
@@ -16,40 +14,6 @@ $ freak <0-10>
 $ freak 1 
 $ freak 10
 `
-
-const freakfile = ".freak.csv"
-
-func homeDir() string {
-	if runtime.GOOS == "windows" {
-		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-		if home == "" {
-			home = os.Getenv("USERPROFILE")
-		}
-		return home
-	}
-	return os.Getenv("HOME")
-}
-
-func fileExists(name string) bool {
-	_, err := os.Stat(name)
-	return !os.IsNotExist(err)
-}
-
-func createOrGetFile(name string) (*os.File, error) {
-	var file *os.File
-	var err error
-	if fileExists(name) {
-		file, err = os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	} else {
-		file, err = os.Create(name)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return file, nil
-}
 
 func main() {
 
@@ -68,21 +32,9 @@ func main() {
 		log.Fatal("quantity must be an integer number")
 	}
 
-	// Create freak file if not exists, otherwise read
-	file, err := createOrGetFile(freakpath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
 	data := []string{time.Now().Format(time.RFC1123), strconv.Itoa(quantity)}
 
-	// Create a new writer
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	// Write csv line at the end
-	err = writer.Write(data)
+	err = WriteToCSV(data, freakpath)
 	if err != nil {
 		log.Fatal(err)
 	}
