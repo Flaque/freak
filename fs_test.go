d49d32dcf9e8c8dc61a06e2e5d5a7dccfa8c87b6
Flaque <econrad@zagmail.gonzaga.edu>
2018-11-02 19:49:43 -0700

    Add tests

---
+++ fs_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	filet "github.com/Flaque/filet"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWritingToCSVCreatesFile(t *testing.T) {
+	defer filet.CleanUp(t)
+
+	dir := filet.TmpDir(t, "")
+
+	err := WriteToCSV([]string{"a", "b", "c"}, dir+"/example.csv")
+	assert.NoError(t, err)
+
+	// Creates the file
+	assert.True(t, filet.Exists(t, dir+"/example.csv"))
+
+	// Populates the file
+	dat, err := ioutil.ReadFile(dir + "/example.csv")
+	assert.NoError(t, err) // sanity check
+	assert.Equal(t, "a,b,c\n", string(dat))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Flaque/freak
 
-require github.com/golang-migrate/migrate v3.5.4+incompatible
+require (
+	github.com/Flaque/filet v0.0.0-20180310200134-dbf95344df50
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/text v0.3.0 // indirect
+)
